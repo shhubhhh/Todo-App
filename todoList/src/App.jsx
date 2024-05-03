521edc4b6dc292e8929b4943fd9fd29719ea9958
shhubhhh <kaushikshubham09@gmail.com>
2024-05-03 13:14:47 +0530

    create home page

---
+++ todoList/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './Home'
+
+function App() {
+  
+
+  return (
+   <>
+   <Home/>
+   </>
+  )
+}
+
+export default App
---
+++ todoList/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div className='create_form'>
+        <input type='text' name="" id ="" placeholder='Create Task'/>
+        <button type='button'>Add</button>
+    </div>
+  )
+}
+
+export default Create
